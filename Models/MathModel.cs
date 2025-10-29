db2fd8fae215f32ccd7c290a3cc296b187e0ded0
Ananth Sai Gurugubelli <AnanthSai.Gurugubelli@tesco.com>
2025-10-29 17:36:10 +0530

    Add project files.

---
+++ Models/MathModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MathOperationApp.Models
+{
+    public class MathModel
+    {
+        public string Expression { get; set; } = string.Empty;
+        public string Result { get; set; } = string.Empty;
+    }
+}
